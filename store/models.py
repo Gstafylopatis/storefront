a410963b36cb3b15211343c91f8a64ea7c3ea4e7
gstafylopatis <gstafylopatis@gmail.com>
2021-10-06 16:45:45 +0300

    Completed Models

---
+++ store/models.py
@@ -0,0 +1,108 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+')  # Use + sign to tell django not create the reverse relationship
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=3)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(
+        Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
+
+
+class Customer(models.Model):
+
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'BRONZE'),
+        (MEMBERSHIP_SILVER, 'SILVER'),
+        (MEMBERSHIP_GOLD, 'GOLD')
+    ]
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+
+class Order(models.Model):
+
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+
+    PAYMENT_STATUS = [
+        (PAYMENT_STATUS_PENDING, 'PENDING'),
+        (PAYMENT_STATUS_COMPLETE, 'COMPLETE'),
+        (PAYMENT_STATUS_FAILED, 'FAILED')
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_STATUS, default=PAYMENT_STATUS_PENDING)
+
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=7, decimal_places=3)
+
+
+# One to One relationship between address and Customer
+# Each Customer only has one address and each address
+# belongs to only one Customer
+# Address class is child of Customer so it should exist
+# after Customer is created
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+
+    # Specify parent
+    # If we dont set primary key to true
+    # Django will create another id key
+    # for each address and then we will be able
+    # to have multiple addresses for one customer
+    # customer = models.OneToOneField(
+    #    Customer, on_delete=models.CASCADE, primary_key=True)
+
+    # If we want to allow each customer to have more than one
+    # address then we need to define OneToManyRelationship
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+
+    # Here we use cascade because if we delete a product
+    # It means that this product has never been ordered
+    # before because orderItem has product protected,
+    # so we delete it from the cart too
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
